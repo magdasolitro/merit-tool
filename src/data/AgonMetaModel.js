74e37fbf630bd91e6b56b617d4c2fd2f456f5d7b
Magdalena <msolitro@fbk.eu>
2026-04-14 16:10:54 +0200

    Removed compliance task

--- src/data/AgonMetaModel.js
+++ src/data/AgonMetaModel.js
@@ -31,12 +31,19 @@ const agonMetaModel = {
             draggable: false
         },
         {
-            id: "compliance-task",
+            id: "compliance-principles",
             type: "circle",
-            position: {x: 4250, y: 200},
-            data: {label: "Compliance Task", background: "black"},
+            position: {x: 3700, y: 200},
+            data: {label: "Compliance Principles", background: "black"},
             draggable: false
-        },
+        }
+        // {
+        //     id: "compliance-task",
+        //     type: "circle",
+        //     position: {x: 4250, y: 200},
+        //     data: {label: "Compliance Task", background: "black"},
+        //     draggable: false
+        // },
 
         // 3rd layer
         {
@@ -60,41 +67,41 @@ const agonMetaModel = {
             data: {label: "Goal Regulation", size: 130},
             draggable: false
         },
-        {
-            id: "task-analysis",
-            type: "circle",
-            position: {x: 3400, y: 500},
-            data: {label: "Task Analysis"},
-            draggable: false
-        },
-        {
-            id: "task-design",
-            type: "circle",
-            position: {x: 4000, y: 500},
-            data: {label: "Task Design"},
-            draggable: false
-        },
-        {
-            id: "task-development",
-            type: "circle",
-            position: {x: 4500, y: 500},
-            data: {label: "Task Development"},
-            draggable: false
-        },
-        {
-            id: "task-deployment",
-            type: "circle",
-            position: {x: 4900, y: 500},
-            data: {label: "Task Deployment"},
-            draggable: false
-        },
-        {
-            id: "task-maintenance",
-            type: "circle",
-            position: {x: 5500, y: 500},
-            data: {label: "Task Maintenance"},
-            draggable: false
-        },
+        // {
+        //     id: "task-analysis",
+        //     type: "circle",
+        //     position: {x: 3400, y: 500},
+        //     data: {label: "Task Analysis"},
+        //     draggable: false
+        // },
+        // {
+        //     id: "task-design",
+        //     type: "circle",
+        //     position: {x: 4000, y: 500},
+        //     data: {label: "Task Design"},
+        //     draggable: false
+        // },
+        // {
+        //     id: "task-development",
+        //     type: "circle",
+        //     position: {x: 4500, y: 500},
+        //     data: {label: "Task Development"},
+        //     draggable: false
+        // },
+        // {
+        //     id: "task-deployment",
+        //     type: "circle",
+        //     position: {x: 4900, y: 500},
+        //     data: {label: "Task Deployment"},
+        //     draggable: false
+        // },
+        // {
+        //     id: "task-maintenance",
+        //     type: "circle",
+        //     position: {x: 5500, y: 500},
+        //     data: {label: "Task Maintenance"},
+        //     draggable: false
+        // },
 
         // 4th layer
         {
@@ -141,146 +148,146 @@ const agonMetaModel = {
             draggable: false
         },
 
-        {
-            id: "purpose-and-scope",
-            type: "circle",
-            position: {x: 3200, y: 800},
-            data: {label: "Definition of AI Purpose and Scope", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "risk-assessment",
-            type: "circle",
-            position: {x: 3350, y: 800},
-            data: {label: "Risk Assessment", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "trustworthiness-requirements",
-            type: "circle",
-            position: {x: 3500, y: 800},
-            data: {label: "Definition of Trustworthiness Requirements", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "stakeholder-engagement",
-            type: "circle",
-            position: {x: 3650, y: 800},
-            data: {label: "Stakeholder Engagement", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "data-collection",
-            type: "circle",
-            position: {x: 3800, y: 800},
-            data: {label: "Data Collection", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "architectural-design",
-            type: "circle",
-            position: {x: 3950, y: 800},
-            data: {label: "Architectural Design", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "threat-modeling",
-            type: "circle",
-            position: {x: 4200, y: 800},
-            data: {label: "Threat Modeling", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "model-selection",
-            type: "circle",
-            position: {x: 4350, y: 800},
-            data: {label: "Model Selection", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "secure-coding",
-            type: "circle",
-            position: {x: 4500, y: 800},
-            data: {label: "Secure Coding", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "model-training",
-            type: "circle",
-            position: {x: 4650, y: 800},
-            data: {label: "Model Training", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "testing",
-            type: "circle",
-            position: {x: 4800, y: 800},
-            data: {label: "Testing", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "model-improv",
-            type: "circle",
-            position: {x: 4950, y: 800},
-            data: {label: "Model Improvement", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "model-validation",
-            type: "circle",
-            position: {x: 5200, y: 800},
-            data: {label: "Model Validation", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "documentation",
-            type: "circle",
-            position: {x: 5350, y: 800},
-            data: {label: "Documentation", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "controlled-rollout",
-            type: "circle",
-            position: {x: 5500, y: 800},
-            data: {label: "Controlled Rollout", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "security-hardening",
-            type: "circle",
-            position: {x: 5650, y: 800},
-            data: {label: "Security Hardening", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "continuous monitoring",
-            type: "circle",
-            position: {x: 5800, y: 800},
-            data: {label: "Continuous Monitoring", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "feedback",
-            type: "circle",
-            position: {x: 5950, y: 800},
-            data: {label: "Feedback-Mechanism", background: "black", isConnectable: true, isChosen: false,},
-            draggable: false
-        },
-        {
-            id: "model-updates",
-            type: "circle",
-            position: {x: 6200, y: 800},
-            data: {label: "Model Updates & Retraining", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
-        {
-            id: "auditing",
-            type: "circle",
-            position: {x: 6350, y: 800},
-            data: {label: "Incident Response & Auditing", background: "black", isConnectable: true, isChosen: true,},
-            draggable: false
-        },
+        // {
+        //     id: "purpose-and-scope",
+        //     type: "circle",
+        //     position: {x: 3200, y: 800},
+        //     data: {label: "Definition of AI Purpose and Scope", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "risk-assessment",
+        //     type: "circle",
+        //     position: {x: 3350, y: 800},
+        //     data: {label: "Risk Assessment", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "trustworthiness-requirements",
+        //     type: "circle",
+        //     position: {x: 3500, y: 800},
+        //     data: {label: "Definition of Trustworthiness Requirements", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "stakeholder-engagement",
+        //     type: "circle",
+        //     position: {x: 3650, y: 800},
+        //     data: {label: "Stakeholder Engagement", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "data-collection",
+        //     type: "circle",
+        //     position: {x: 3800, y: 800},
+        //     data: {label: "Data Collection", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "architectural-design",
+        //     type: "circle",
+        //     position: {x: 3950, y: 800},
+        //     data: {label: "Architectural Design", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "threat-modeling",
+        //     type: "circle",
+        //     position: {x: 4200, y: 800},
+        //     data: {label: "Threat Modeling", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "model-selection",
+        //     type: "circle",
+        //     position: {x: 4350, y: 800},
+        //     data: {label: "Model Selection", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "secure-coding",
+        //     type: "circle",
+        //     position: {x: 4500, y: 800},
+        //     data: {label: "Secure Coding", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "model-training",
+        //     type: "circle",
+        //     position: {x: 4650, y: 800},
+        //     data: {label: "Model Training", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "testing",
+        //     type: "circle",
+        //     position: {x: 4800, y: 800},
+        //     data: {label: "Testing", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "model-improv",
+        //     type: "circle",
+        //     position: {x: 4950, y: 800},
+        //     data: {label: "Model Improvement", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "model-validation",
+        //     type: "circle",
+        //     position: {x: 5200, y: 800},
+        //     data: {label: "Model Validation", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "documentation",
+        //     type: "circle",
+        //     position: {x: 5350, y: 800},
+        //     data: {label: "Documentation", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "controlled-rollout",
+        //     type: "circle",
+        //     position: {x: 5500, y: 800},
+        //     data: {label: "Controlled Rollout", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "security-hardening",
+        //     type: "circle",
+        //     position: {x: 5650, y: 800},
+        //     data: {label: "Security Hardening", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "continuous monitoring",
+        //     type: "circle",
+        //     position: {x: 5800, y: 800},
+        //     data: {label: "Continuous Monitoring", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "feedback",
+        //     type: "circle",
+        //     position: {x: 5950, y: 800},
+        //     data: {label: "Feedback-Mechanism", background: "black", isConnectable: true, isChosen: false,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "model-updates",
+        //     type: "circle",
+        //     position: {x: 6200, y: 800},
+        //     data: {label: "Model Updates & Retraining", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
+        // {
+        //     id: "auditing",
+        //     type: "circle",
+        //     position: {x: 6350, y: 800},
+        //     data: {label: "Incident Response & Auditing", background: "black", isConnectable: true, isChosen: true,},
+        //     draggable: false
+        // },
 
         // final layer
         {
